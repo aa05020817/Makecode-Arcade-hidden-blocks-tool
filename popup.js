605fb028d104a75c084c9b1efb86d2939c246079
Len_AshBell <jason1022liu@gmail.com>
2019-09-27 16:18:51 +0800

    fix wrong field name bug

--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@ var getSelectedTab = (tab) => {
     var tabId = tab.id;
     var sendMessage = (messageObj) => chrome.tabs.sendMessage(tabId, messageObj);
     document.getElementById('hidden').addEventListener('click', () => sendMessage({ action: 'HIDDEN' }));
-    document.getElementById('reset').addEventListener('click', () => sendMessage({ aciton: 'RESET' }))
+    document.getElementById('reset').addEventListener('click', () => sendMessage({ action: 'RESET' }))
 }
 // chrome.tabs.query(active, getSelectedTab);
 chrome.tabs.getSelected(null, getSelectedTab);
