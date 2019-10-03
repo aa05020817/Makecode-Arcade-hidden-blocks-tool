de8df3215119b85562e5b7a6304c972d946e9500
aa05020817 <aa05020817@gmail.com>
2019-10-03 20:32:18 +0800

    select test

--- execute.js
+++ execute.js
@@ -3,6 +3,12 @@ const h_blocklyToolbox = () => {
 };
 const h_blocklyReset = () => {
     document.body.getElementsByClassName('blocklyToolboxDiv')[0].style.display = 'block';
+};
+const h_cha = () => {
+    document.getElementById('CDBZU)A.7xwctt;x~sUj').style.display = 'none';
+};
+const r_cha = () => {
+    document.getElementById('CDBZU)A.7xwctt;x~sUj').style.display = 'block';
 }
 const onMessage = (message) => {
     switch (message.action) {
@@ -12,6 +18,12 @@ const onMessage = (message) => {
         case 'RESET':
             h_blocklyReset();
             break;
+        case 'HIDDEN_CHA':
+            h_cha();
+            break;
+        case 'RESET_CHA':
+            r_cha();
+            break;    
         default:
             break;
     }
