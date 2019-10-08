cb26c3872f53b8f8c0a10f79a1bfe9d11588d7f0
aa05020817 <aa05020817@gmail.com>
2019-10-08 11:07:58 +0800

    new

--- popup.js
+++ popup.js
@@ -2,13 +2,9 @@ var getSelectedTab = (tab) => {
     var tabId = tab.id;
     var sendMessage = (messageObj) => chrome.tabs.sendMessage(tabId, messageObj);
     document.getElementById('hidden').addEventListener('click', () => sendMessage({ action: 'HIDDEN' }));
-<<<<<<< HEAD
-    document.getElementById('reset').addEventListener('click', () => sendMessage({ action: 'RESET' }))
-=======
     document.getElementById('reset').addEventListener('click', () => sendMessage({ action: 'RESET' }));
     document.getElementById('hidden_cha').addEventListener('click', () => sendMessage({ action: 'HIDDEN_CHA'}));
     document.getElementById('reset_cha').addEventListener('click', () => sendMessage({ action: 'RESET_CHA'}))
->>>>>>> select test
 }
 // chrome.tabs.query(active, getSelectedTab);
 chrome.tabs.getSelected(null, getSelectedTab);
